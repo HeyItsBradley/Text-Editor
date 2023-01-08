709ef6979ac0cba23d82419c552eea7422b0a7cc
HeyItsBradley <bradleyrodriguez77@gmail.com>
2023-01-08 11:16:30 -0700

    added content to the install.js file

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,27 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  window.deferredPrompt = event;
+  butInstall.style.display = "block";
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener("click", async () => {
+  butInstall.style.display = "none";
+  window.deferredPrompt.prompt();
+  const userChoice = await window.deferredPrompt.userChoice;
+  window.deferredPrompt = null;
+  if (userChoice.outcome === "accepted") {
+    console.log("User wants to install JATE");
+  } else {
+    console.log("User does not want to install JATE :(");
+  }
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {
+  console.log("Jate has been installed !");
+});
